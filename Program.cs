c02375f9c5447330a33d43551aae0715c0f1548e
ToniaRomaniuk <roxyromanik@gmail.com>
2023-02-13 20:09:40 +0300

    HW5

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = CreateArray(5, 100, 999);
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine($"Количество четных чисел в массиве: {GetCount(array)}");
+
+int [] CreateArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetCount(int[] array)
+{
+    int count = 0;
+
+   for (int i=0; i<array.Length; i++)
+   {
+    if (array[i] % 2 == 0)
+    count++;
+   }
+   return count;
+}
+
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+    
+int[] SumArray = CreateArray(4, 3, 23);                            
+Console.WriteLine(String.Join(" ", SumArray));
+Console.WriteLine($"Сумма элементов на нечетных позициях: {NegativSum(SumArray)}");
+
+int NegativSum (int[] array)
+{
+   int sum = 0;
+   
+   for (int i=0; i<array.Length; i++)
+   {
+    if (i % 2 != 0)
+    sum += array[i];
+   }
+   return sum;
+}
+
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+// [3 7 22 2 78] -> 76
+
+double[] NegativArr = DoubleArray(10, 0, 10);
+Console.WriteLine(String.Join(" ", NegativArr));
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {Diff(NegativArr)}");
+
+double[] DoubleArray(int size, int minValue, int maxValue)  
+{
+    double[] res = new double[size];
+
+    var rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = Convert.ToDouble(rand.Next(0, 100)/10.0);
+    }
+    return res;
+}
+
+double Diff(double[] Array)  
+
+{
+    double min = Array[0];
+    double max = Array[0];
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] < min) min = Array[i];
+        if (Array[i] > max) max = Array[i];
+    }
+    return max - min;
+}
+
